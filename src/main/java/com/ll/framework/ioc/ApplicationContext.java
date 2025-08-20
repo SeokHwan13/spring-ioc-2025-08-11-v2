01883ac57470eb03062a81a059c71a7f9d016631
SeokHwan13 <seokhwan1113@naver.com>
2025-08-20 18:03:27 +0900

    테스트 통과

--- src/main/java/com/ll/framework/ioc/ApplicationContext.java
+++ src/main/java/com/ll/framework/ioc/ApplicationContext.java
@@ -1,14 +1,50 @@
 package com.ll.framework.ioc;
 
-public class ApplicationContext {
-    public ApplicationContext(String basePackage) {
+import com.ll.framework.ioc.annotations.Component;
+import com.ll.standard.util.Ut;
+import org.reflections.Reflections;
 
+import java.lang.reflect.Constructor;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class ApplicationContext {
+    private final Map<String, Object> beans = new HashMap<>();
+    private final Reflections  reflections;
+
+    public ApplicationContext(String basePackage) {
+        reflections = new Reflections(basePackage);
     }
 
     public void init() {
+        Set<Class<?>> classes = reflections.getTypesAnnotatedWith(Component.class)
+                .stream()
+                .filter(c -> !c.isAnnotation())
+                .collect(Collectors.toSet());
+
+        for (Class<?> clazz : classes) {
+            try {
+                Constructor<?>[] constructors = clazz.getDeclaredConstructors();
+                Constructor<?> constructor = constructors[0];
+
+                Class<?>[] paramTypes = constructor.getParameterTypes();
+                Object[] params = Arrays.stream(paramTypes)
+                        .map(paramType -> beans.get(Ut.str.lcfirst(paramType.getSimpleName())))
+                        .toArray();
+
+                Object instance = constructor.newInstance(params);
+
+                String name = Ut.str.lcfirst(clazz.getSimpleName());
+
+                beans.put(name, instance);
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     public <T> T genBean(String beanName) {
-        return null;
+        return (T) beans.get(beanName);
     }
 }
