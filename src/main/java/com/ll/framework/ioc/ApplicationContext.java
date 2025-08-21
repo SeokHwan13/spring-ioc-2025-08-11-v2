445b9228fff6bdc0b7353af9412ea61d8f69f4ae
SeokHwan13 <seokhwan1113@naver.com>
2025-08-21 15:25:38 +0900

    빈 생성 순서 오류 수정

--- src/main/java/com/ll/framework/ioc/ApplicationContext.java
+++ src/main/java/com/ll/framework/ioc/ApplicationContext.java
@@ -5,12 +5,16 @@ import com.ll.standard.util.Ut;
 import org.reflections.Reflections;
 
 import java.lang.reflect.Constructor;
-import java.util.*;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 import java.util.stream.Collectors;
+
 
 public class ApplicationContext {
     private final Map<String, Object> beans = new HashMap<>();
-    private final Reflections  reflections;
+    private final Reflections reflections;
 
     public ApplicationContext(String basePackage) {
         reflections = new Reflections(basePackage);
@@ -23,28 +27,36 @@ public class ApplicationContext {
                 .collect(Collectors.toSet());
 
         for (Class<?> clazz : classes) {
-            try {
-                Constructor<?>[] constructors = clazz.getDeclaredConstructors();
-                Constructor<?> constructor = constructors[0];
+            createBean(clazz);
+        }
+    }
 
-                Class<?>[] paramTypes = constructor.getParameterTypes();
-                Object[] params = Arrays.stream(paramTypes)
-                        .map(paramType -> beans.get(Ut.str.lcfirst(paramType.getSimpleName())))
-                        .toArray();
+    private Object createBean(Class<?> clazz) {
+        String name = Ut.str.lcfirst(clazz.getSimpleName());
 
-                Object instance = constructor.newInstance(params);
+        // 이미 만들어진 빈이면 반환
+        if (beans.containsKey(name)) {
+            return beans.get(name);
+        }
 
-                String name = Ut.str.lcfirst(clazz.getSimpleName());
+        try {
+            Constructor<?> constructor = clazz.getDeclaredConstructors()[0];
 
-                beans.put(name, instance);
+            // 생성자 파라미터들을 재귀적으로 해결
+            Object[] params = Arrays.stream(constructor.getParameterTypes())
+                    .map(this::createBean) // 재귀 호출
+                    .toArray();
 
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+            Object bean = constructor.newInstance(params);
+            beans.put(name, bean);
+            return bean;
+
+        } catch (Exception e) {
+            throw new RuntimeException("빈 생성 실패: " + clazz, e);
         }
     }
 
     public <T> T genBean(String beanName) {
-        return (T) beans.get(beanName);
+        return  (T) beans.get(beanName);
     }
 }
